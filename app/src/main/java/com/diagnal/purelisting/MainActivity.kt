3814f7644b0581c081adc8726c811228f5cf39b5
manishChouddhary <choudharymanish629@gmail.com>
2020-04-30 19:01:35 +0530

    Updated the app center related files also generated the release build

--- app/src/main/java/com/diagnal/purelisting/MainActivity.kt
+++ app/src/main/java/com/diagnal/purelisting/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.diagnal.purelisting
 
 import android.os.Bundle
-import android.view.Menu
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
 import com.diagnal.purelisting.content.ContentListFragment
 import com.diagnal.purelisting.extentions.replace
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "4c1f2231-a557-43db-be75-5157d068696f",
+            Analytics::class.java, Crashes::class.java
+        )
         setContentView(R.layout.activity_main)
         setupToolbar()
         replace(ContentListFragment(), R.id.container, false, ContentListFragment::class.java.canonicalName)
